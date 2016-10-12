911d155765e0f3bd1d18d5689fe662e7ca74861b
Wil Pirino <wpirino1@gmail.com>
2016-10-12 15:37:21 -0700

    quotes route working

--- models/quote.js
+++ models/quote.js
@@ -5,20 +5,27 @@ const User = require('./user.js');
 const QuoteHear = require('./quoteHear.js');
 
 module.exports = {
-  qoutes: function () {
-    return new new Promise(function(resolve, reject) {
+  quotes: function () {
+    return new Promise(function(resolve, reject) {
       const client = database.newClient();
-      client.query("SELECT * " +
+      client.query("SELECT quotes.*, " +
+                   "said_by_users.first_name AS said_by_first_name, " +
+                   "said_by_users.last_name AS said_by_last_name, " +
+                   "said_by_users.phone_number AS said_by_phone_number, " +
+                   "heard_by_users.first_name AS heard_by_first_name, " +
+                   "heard_by_users.last_name AS heard_by_last_name, " +
+                   "heard_by_users.phone_number AS heard_by_phone_number " +
                    "FROM quotes " +
                    "JOIN users AS said_by_users " +
                    "ON quotes.said_by_user_id = said_by_users.id " +
                    "LEFT JOIN quote_hears " + // a quote no one heard??
                    "ON quote_hears.quote_id = quotes.id " +
-                   "JOIN users AS heard_by_users " +
-                   "ON quote_hears.heard_by_user_id = heard_by_users.id")
+                   "LEFT JOIN users AS heard_by_users " +
+                   "ON quote_hears.heard_by_user_id = heard_by_users.id " +
+                   "ORDER BY quotes.id")
             .then(function (results) {
-              const parsedRows = parseQuoteRowAfterJoin(results.rows);
-              resolve(parsedRows);
+              const quotes = aggregateHeardByUsers(results.rows);
+              resolve(quotes);
               client.end();
             }).catch(function (err) {
               reject(err);
@@ -74,7 +81,41 @@ module.exports = {
   }
 }
 
-function parseQuoteRowAfterJoin (rows) {
-  console.log(rows);
-  return rows;
+function groupRows (rows) {
+  let groups = [], currGroup = [];
+  for (let i = 0; i < rows.length; i++) {
+    currGroup.push(rows[i]);
+    if (i === rows.length - 1 || rows[i].id !== rows[i + 1].id) {
+      groups.push(currGroup);
+      currGroup = [];
+    }
+  }
+  return groups;
+}
+
+function aggregateHeardByUsers (quotes) {
+  const quoteGroups = groupRows(quotes);
+  const aggregatedQuotes = [];
+  for (let i = 0; i < quoteGroups.length; i++) {
+    const quoteGroup = quoteGroups[i];
+    const heardByUsers = [];
+    for (var k = 0; k < quoteGroup.length; k++) {
+      heardByUsers.push({
+        firstName: quoteGroup[k].heard_by_first_name,
+        lastName: quoteGroup[k].heard_by_last_name,
+        phoneNumber: quoteGroup[k].heard_by_phone_number
+      });
+    }
+    aggregatedQuotes.push({
+      text: quoteGroup[0].text,
+      saidAt: quoteGroup[0].said_at,
+      saidBy: {
+        firstName: quoteGroup[0].said_by_first_name,
+        lastName: quoteGroup[0].said_by_last_name,
+        phoneNumber: quoteGroup[0].said_by_phone_number
+      },
+      heardBy: heardByUsers
+    });
+  }
+  return aggregatedQuotes;
 }
