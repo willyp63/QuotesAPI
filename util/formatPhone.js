2dc013ca91459496fb507dbeb56af419c1abd256
Wil Pirino <wpirino1@gmail.com>
2016-10-19 11:50:35 -0700

    removed phone number formatting

--- util/formatPhone.js
+++ util/formatPhone.js
@@ -3,9 +3,11 @@
 const phoneFormatter = require('phone');
 
 module.exports = function (number) {
-  const formatted = phoneFormatter(number)[0];
-  if (formatted.length != 12) {
-    throw new Error("That's not a real phone number!");
-  }
-  return formatted;
+  return number;
+  
+  // const formatted = phoneFormatter(number)[0];
+  // if (formatted.length != 12) {
+  //   throw new Error("That's not a real phone number!");
+  // }
+  // return formatted;
 }
